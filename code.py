157b7e73f12e8ce6a2db7ce105ffd0221df73fa9
Anthony <aramirez1@ucvts.org>
2020-01-09 10:08:28 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,10 @@
 print ("Welcome to your closet!")
 print ("Are you ready to get your outfit?")
 answer = input ("Enter yes or no: ")
-if answer != "yes" or "no":
-    if answer == "no":
-        print ("Leaving the game")
-        exit()
-    elif answer =="yes":
-        print("Are you a boy or a girl?")
-    else:
-        print("Please enter yes or no")
+if answer == "no":
+    print ("Leaving the game")
+    exit()
+elif answer =="yes":
+    print("Are you a boy or a girl?")
+while answer != "yes" or answer != "no":
+    input("Please enter yes or no")
