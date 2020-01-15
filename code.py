d4b7ed71fd27948e01e75935a1074f266aca414c
Anthony <aramirez1@ucvts.org>
2020-01-15 10:48:25 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,20 @@
+import random
 print ("Welcome to your closet!")
 print ("Are you ready to get your outfit?")
 answer = input ("Enter yes or no: ")
 boy_sportybottom = ["blue basketball shorts", "red basketball shorts", "white basketball shorts"]
 boy_sportytop = ["blue t-shirt", "red t-shirt", "black t-shirt"]
+boy_sportyshoes = ["red sneakers", "black sneakers", "white sneakers"]
+casual_top = ["brown shirt", "black shirt", "white shirt"]
+casual_bottom = ["grey sweatpants", "blue jeans", "black jeans", "black sweatpants"]
+casual_shoes =["gucci slides", "black air forces", "white air forces"]
+boy_fancysuit = ["black suit", "navy blue suit", "grey suit"]
+boy_fancyshoes = ["black dress shoes", "brown dress shoes"]
+girl_sportybottom = ["blue leggings", "black leggings", "gray leggings"]
+girl_sportytop = ["blue t-shirt", "red t-shirt", "pink t-shirt"]
+girl_sportyshoes = ["blue sneakers", "black sneakers", "white sneakers"]
+girl_fancydress = ["white dress", "red dress", "purple dress"]
+girl_fancyheels = ["white heels", "black heels"]
 
 while answer not in ["yes", "no"]:
     answer = input("Please answer yes or no ")
@@ -23,7 +35,7 @@ clothes = input("sporty, casual, or fancy?")
 while clothes not in ["sporty", "casual", "fancy"]:
     clothes = input("Please answer sporty, casual, or fancy")
 if clothes == "sporty":
-    print("sport")
+    print(random.choice(boy_sportybottom))
 elif clothes == "casual":
     print("cas")
 else:
